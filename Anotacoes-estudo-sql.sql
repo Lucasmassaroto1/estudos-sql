e87fe91736dc4c8d382e8febdceffcaf90bc8d99
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-26 10:25:21 -0300

    Unindo comentarios

--- Anotacoes-estudo-sql.sql
+++ Anotacoes-estudo-sql.sql
@@ -1,19 +1,19 @@
-/* 
-_____________TITULO_____________
-    » 
-    » 
-    » 
-_____________TITULO_____________
-    » 
-    » 
-    » 
-_____________TITULO_____________
-    » 
-    » 
-    » 
-*/
+/*
+-- CREATE TABLE tb_cursos( id_curso int not null, imagem_curso varchar(100) not null, nome_curso char(50) not null, resumo text null, data_cadastro datetime not null, ativo boolean default true, investimento float(8, 2) default 0 );
 
-/* 
+_____________TITULO_____________
+    » 
+    » 
+    » 
+_____________TITULO_____________
+    » 
+    » 
+    » 
+_____________TITULO_____________
+    » 
+    » 
+    » 
+
 _____________CAMPOS DE TEXTO_____________
     » Text (tamanho variável que armazena uma grande quantidade de caracteres)
     » Varchar (tamanho variável que armazena de 0 até 255 caracteres)
@@ -25,6 +25,11 @@ _____________CAMPOS DE DATA E HORA_____________
     » Date (data no formato YYYY/mm/dd)
     » Time (hora)
     » Datetime (combinação de date e time em um mesmo campo)
-*/
 
--- CREATE TABLE tb_cursos( id_curso int not null, imagem_curso varchar(100) not null, nome_curso char(50) not null, resumo text null, data_cadastro datetime not null, ativo boolean default true, investimento float(8, 2) default 0 );
+_____________ALTER TABLE_____________
+    » ADD (permite a inclusão de uma nova coluna em uma tabela)
+    » CHANGE (permite a alteração do nome de uma coluna e de suas propriedades, como por ex o tipo)
+    » DROP (permite a remoção de uma coluna da tabela)
+
+
+*/
